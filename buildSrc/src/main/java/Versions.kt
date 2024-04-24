aed09263f053c7c393102fd6047afc2d5cc9d3aa
gggmg <cont96roller@gmail.com>
2024-04-24 13:17:48 +0900

    chore: gradle dependencies version 선언 방식 변경

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -10,10 +10,32 @@ object Versions {
     val JAVA_VERSION = JavaVersion.VERSION_1_8
     const val JVM_TARGET = "1.8"
 
-    //app version
+    // app version
     const val VERSION_CODE = 1
     const val VERSION_NAME = "1.0"
 
+    // kotlin
+    const val COROUTINES = "1.6.4"
 
+    // jetpack
+    const val CORE = "1.13.0"
+    const val APP_COMPAT = "1.6.1"
+    const val CONSTRAINT_LAYOUT = "2.1.4"
+    const val NAVIGATION = "2.7.7"
 
+    // google
+    const val MATERIAL = "1.11.0"
+    const val GSON = "2.10.1"
+
+    // hilt
+    const val HILT = "2.51.1"
+
+    // 3rd
+    const val OKHTTP_BOM = "4.12.0"
+    const val RETROFIT = "2.9.0"
+
+    // test
+    const val JUNIT_TEST = "4.13.2"
+    const val JUNIT = "1.1.5"
+    const val ESPRESSO = "3.5.1"
 }
--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -33,12 +33,31 @@ android {
 }
 
 dependencies {
-
     implementation(project(":domain"))
-    implementation("androidx.core:core-ktx:1.13.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("androidx.core:core-ktx:${Versions.CORE}")
+
+    // hilt
+    implementation("com.google.dagger:dagger:${Versions.HILT}")
+    implementation("com.google.dagger:hilt-android:${Versions.HILT}")
+
+    // coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.COROUTINES}")
+
+    // okHttp
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:${Versions.OKHTTP_BOM}"))
+    implementation("com.squareup.okhttp3:okhttp")
+    implementation("com.squareup.okhttp3:logging-interceptor")
+
+    // retrofit
+    implementation("com.squareup.retrofit2:converter-gson:${Versions.RETROFIT}")
+    implementation("com.squareup.retrofit2:retrofit:${Versions.RETROFIT}")
+
+    // gson
+    implementation("com.google.code.gson:gson:${Versions.GSON}")
+
+    // test
+    testImplementation("junit:junit:${Versions.JUNIT_TEST}")
+    androidTestImplementation("androidx.test.ext:junit:${Versions.JUNIT}")
+    androidTestImplementation("androidx.test.espresso:espresso-core:${Versions.ESPRESSO}")
 }
