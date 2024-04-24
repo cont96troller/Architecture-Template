db01cbf543d530a359d80b97943ed34632aa33f9
gggmg <cont96roller@gmail.com>
2024-04-24 13:09:48 +0900

    refactor: gradle에 선언된 버전 Versions.kt로 관리하도록 수정

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,2 +1,19 @@
-class Versions {
+import org.gradle.api.JavaVersion
+
+object Versions {
+
+    // Sdk and tools
+    const val COMPILE_SDK = 34
+    const val MIN_SDK = 24
+    const val TARGET_SDK = 34
+
+    val JAVA_VERSION = JavaVersion.VERSION_1_8
+    const val JVM_TARGET = "1.8"
+
+    //app version
+    const val VERSION_CODE = 1
+    const val VERSION_NAME = "1.0"
+
+
+
 }
