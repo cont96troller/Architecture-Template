b5bd671c7220134d8dd20b3b62fdf7a634a7334d
gggmg <cont96roller@gmail.com>
2024-04-26 17:10:17 +0900

    Fragment에서 viewModel을 통해 api콜을 시작하고, currentWeather 변수를 collect하도록 구현

--- app/src/main/java/com/yoohyun/sample/CurrentWeatherFragment.kt
+++ app/src/main/java/com/yoohyun/sample/CurrentWeatherFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.yoohyun.sample.databinding.FragmentFirstBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -26,8 +30,7 @@ class CurrentWeatherFragment : Fragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
 
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
@@ -38,8 +41,27 @@ class CurrentWeatherFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        collectData()
+
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+    private fun collectData() {
+        collectCurrentWeatherData()
+    }
+
+    private fun collectCurrentWeatherData() {
+        viewModel.getCurrentWeather(37.749, 128.87)
+        lifecycleScope.launch {
+            viewModel.currentWeather.collectLatest { currentWeather ->
+
+                currentWeather?.description?.let { description ->
+                    Toast.makeText(requireContext(), description, Toast.LENGTH_SHORT).show()
+                }
+
+            }
         }
     }
 
